23511271cda2847128ec48ad92d024e9012e89b8
Daniel Konrath <konrath@student.tugraz.at>
2021-02-26 22:37:47 +0100

    added console inputs again

--- main.py
+++ main.py
@@ -24,72 +24,6 @@ def draw_strip(e, strip_arr, mode_arr):
         strip.show()
         time.sleep(STANDARD_TICK_LENGTH*10)
 
-'''def command_handler(if_input, input_buffer):
-    if if_input == 'ch_down':
-        index = (index+1)%len(dispatcher)
-        e.set()
-        channel_thr.join()
-        e.clear()
-        global_settings.reset()
-        channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-        channel_thr.start()
-    elif if_input == 'ch_up':
-        index = (index-1)%len(dispatcher)
-        e.set()
-        channel_thr.join()
-        e.clear()
-        global_settings.reset()
-        channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-        channel_thr.start()
-    elif if_input == 'var_up':
-        global_settings.variation += 1
-    elif if_input == 'var_down':
-        global_settings.variation -= 1
-    elif if_input == 's_up':
-        global_settings.tick_length = round(min(global_settings.tick_length+0.0002,0.005),4)
-        print("speed: ",  global_settings.tick_length)
-    elif if_input == 's_down':
-        global_settings.tick_length = round(max(global_settings.tick_length-0.0002,0.0002),4)
-        print("speed: ",  global_settings.tick_length)
-    elif if_input == 'b_up':
-        global_settings.brightness = round(min(1, global_settings.brightness+0.1),1)
-        print("brightness: ", global_settings.brightness)
-    elif if_input == 'b_down':
-        global_settings.brightness = round(max(0, global_settings.brightness-0.1),1)
-        print("brightness: ", global_settings.brightness)
-    elif if_input == 'm_up':
-        global_settings.mode = (global_settings.mode+1)%len(mode_dispatcher)
-        mode_e.set()
-        mode_thr.join()
-        mode_e.clear()
-        mode_thr = threading.Thread(target=mode_dispatcher[global_settings.mode], args=(mode_e,mode_arr, global_settings))
-        mode_thr.start()
-    elif if_input == 'm_down':
-        global_settings.mode = (global_settings.mode-1)%len(mode_dispatcher)
-        mode_e.set()
-        mode_thr.join()
-        mode_e.clear()
-        mode_thr = threading.Thread(target=mode_dispatcher[global_settings.mode], args=(mode_e,mode_arr, global_settings))
-        mode_thr.start()
-    elif if_input == 'ok':
-        if len(input_buffer) == 0:
-            print("no channel number")
-            return
-        elif len(input_buffer) == 1:
-            new_ch = int(input_buffer)
-        else:
-            new_ch = int(input_buffer[-2:])
-            
-        if new_ch >= 0 and new_ch < len(dispatcher):
-            index = new_ch
-            e.set()
-            channel_thr.join()
-            e.clear()
-            global_settings.reset()
-            channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-            channel_thr.start()
-        else:
-            print("invalid channel")'''
         
 def sensor_handler(e, cmd):
     input_buffer = ""
@@ -105,7 +39,26 @@ def sensor_handler(e, cmd):
                 cmd[0] = (if_input, input_buffer)
                 input_buffer = ""
         time.sleep(STANDARD_TICK_LENGTH*10)
+
+def console_input(e, cmd):
+    while True:
+        if e.isSet():
+            return
         
+        new_input = input("Enter your command: ")
+        new_input_split = new_input.split()
+        
+        if len(new_input_split) == 1:
+            cmd[0] = (new_input_split[0], 0)
+        elif len(new_input_split) == 2:
+            if new_input_split[1].isnumeric():
+                cmd[0] = (new_input_split[0], new_input_split[1])
+            else:
+                cmd[0] = (new_input_split[0], 0)
+        else:
+            print("invalid console input")
+        
+        time.sleep(STANDARD_TICK_LENGTH * 100)
             
 if __name__ == '__main__':
     strip = Adafruit_NeoPixel(180, 18, 800000, 10, False, 255, 0)
@@ -181,6 +134,9 @@ if __name__ == '__main__':
     
     sensor_thr = threading.Thread(target=sensor_handler, args=(sensor_handler_e, cmd))
     sensor_thr.start()
+    
+    console_thr = threading.Thread(target=console_input, args=(sensor_handler_e, cmd)) #use same event
+    console_thr.start()
     
     while True:
         if cmd[0][0] is not "":
@@ -285,57 +241,7 @@ if __name__ == '__main__':
                 print("color", color_index, " changed to ", new_col)
         elif input_cmd == 'reset':
             global_settings.reset()
-        elif input_cmd == 'off':
-            index = 0
-            e.set()
-            channel_thr.join()
-            e.clear()
-            global_settings.reset()
-            channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-            channel_thr.start()
-                
-    '''while True:
-        a = input()
-        if a == 'd':
-            index = (index+1)%len(dispatcher)
-            e.set()
-            channel_thr.join()
-            e.clear()
-            global_settings.reset()
-            channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-            channel_thr.start()
-        elif a == 'a':
-            index = (index-1)%len(dispatcher)
-            e.set()
-            channel_thr.join()
-            e.clear()
-            global_settings.reset()
-            channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-            channel_thr.start()
-        elif a == 'w':
-            global_settings.variation += 1
-        elif a == 's':
-            global_settings.variation -= 1
-        elif a == 'j':
-            global_settings.tick_length = round(min(global_settings.tick_length+0.0002,0.005),4)
-            print("speed: ",  global_settings.tick_length)
-        elif a == 'k':
-            global_settings.tick_length = round(max(global_settings.tick_length-0.0002,0.0002),4)
-            print("speed: ",  global_settings.tick_length)
-        elif a == 'q':
-            global_settings.brightness = round(min(1, global_settings.brightness+0.1),1)
-            print("brightness: ", global_settings.brightness)
-        elif a == 'e':
-            global_settings.brightness = round(max(0, global_settings.brightness-0.1),1)
-            print("brightness: ", global_settings.brightness)
-        elif a == 'm':
-            global_settings.mode = (global_settings.mode+1)%len(mode_dispatcher)
-            mode_e.set()
-            mode_thr.join()
-            mode_e.clear()
-            mode_thr = threading.Thread(target=mode_dispatcher[global_settings.mode], args=(mode_e,mode_arr, global_settings))
-            mode_thr.start()
-        elif a == 'x':
+        elif input_cmd == 'off' and input_number == '999':
             e.set()
             mode_e.set()
             draw_e.set()
@@ -345,52 +251,13 @@ if __name__ == '__main__':
             mode_thr.join()
             draw_thr.join()
             sensor_thr.join()
+            console_thr.join()
             break
-        elif 'c' in a: # valid input c103, c13 for primary color and third element in dict
-            if len(a) < 3 or len(a) > 4:
-                print("invalid command")
-                continue
-            if a[0] is not 'c':
-                print("invalid command")
-                continue 
-            try:
-                temp_a = a[1]
-                temp_b = a[2:len(a)]
-                color_index = int(temp_a)
-                color_dict_index = int(temp_b)
-                
-                if color_index < 1 or color_index > 3:
-                    print("invalid command")
-                    continue
-                if color_dict_index < 0 or color_dict_index >= len(color_dict):
-                    print("invalid command")
-                    continue
-                    
-                if color_index == 1:
-                    global_settings.color1 = color_dict_index
-                    print("color", color_index, " changed to ", color_dict_index)
-                elif color_index == 2:
-                    global_settings.color2 = color_dict_index
-                    print("color", color_index, " changed to ", color_dict_index)
-                elif color_index == 3:
-                    global_settings.color3 = color_dict_index
-                    print("color", color_index, " changed to ", color_dict_index)
-            except:
-                print("invalid command")
-                continue
-        else:
-            try:
-                new_ch = int(a)
-                if new_ch >= 0 and new_ch < len(dispatcher):
-                    index = new_ch
-                    e.set()
-                    channel_thr.join()
-                    e.clear()
-                    global_settings.reset()
-                    channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
-                    channel_thr.start()
-            except:
-                print("invalid command")
-                continue'''
-                
-            
+        elif input_cmd == 'off':
+            index = 0
+            e.set()
+            channel_thr.join()
+            e.clear()
+            global_settings.reset()
+            channel_thr = threading.Thread(target=dispatcher[index], args=(e,strip_arr, global_settings))
+            channel_thr.start()   
